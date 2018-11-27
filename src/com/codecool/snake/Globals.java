c0fe1ee9c1b720e639c89c658f206baf2aa95c56
doraKoreny <dorakoreny@gmail.com>
2018-11-27 13:06:33 +0100

    choose a random pokemon image for the enemies

--- src/com/codecool/snake/Globals.java
+++ src/com/codecool/snake/Globals.java
@@ -4,9 +4,15 @@ import com.codecool.snake.resources.Resources;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
+import java.util.Random;
+
 // class for holding all static stuff
 public class Globals {
     private static Globals instance = null;
+
+
+    // D: pokemon enemies
+    public String[] ENEMIES = {"pikachu.png", "bullbasaur.png", "charmander.png", "squirtle.png" };
 
     public static final double WINDOW_WIDTH = 1000;
     public static final double WINDOW_HEIGHT = 700;
@@ -27,12 +33,19 @@ public class Globals {
         this.gameLoop = gameLoop;
     }
 
+    // D: get random enemies picture
+    public String getRandomEnemyImage() {
+        Random random = new Random();
+        String randomEnemy = ENEMIES[random.nextInt(ENEMIES.length)];
+        return randomEnemy;
+    }
+
     public void setupResources() {
         resources = new Resources();
-        resources.addImage("SnakeHead", new Image("snake_head.png"));
+        resources.addImage("SnakeHead", new Image("caterpie.png"));
         resources.addImage("SnakeBody", new Image("snake_body.png"));
-        resources.addImage("SimpleEnemy", new Image("simple_enemy.png"));
-        resources.addImage("PowerUpBerry", new Image("powerup_berry.png"));
+        resources.addImage("SimpleEnemy", new Image(getRandomEnemyImage()));
+        resources.addImage("PowerUpBerry", new Image("pokeball.png"));
     }
 
     public Image getImage(String name) { return resources.getImage(name); }
